e2bb29cfe50bbdc95567dce06fa12e9f9a258eba
root <root@Elyes.>
2025-08-27 10:22:05 +0100

    sha validator

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ "$(sha256sum "$1" | cut -d ' ' -f1 | tr '[:upper:]' '[:lower:]')" = "$(echo "$2" | tr '[:upper:]' '[:lower:]')" ] && echo ok || echo invalid
+echo "$2 $1" | sha256sum -c
